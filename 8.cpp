3018ea59a455f12082f578d553fe957b6601a9c5
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 10:40:46 -0400

    Loops
    do while

---
+++ 8.cpp
@@ -0,0 +1,20 @@
+// LOOPS
+// DO WHILE
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    
+    int i=20;
+    do
+    {
+        cout<<" "<<i;
+        i--;
+    } while (i>0);
+    
+    cin.get();
+    return 0;
+}
