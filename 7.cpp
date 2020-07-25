e74019562d3281063341738780df7a6b247a9208
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 10:38:18 -0400

    Loops-WHILE
    while

---
+++ 7.cpp
@@ -0,0 +1,19 @@
+// LOOPS
+// WHILE
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    // while loop
+    int i=20;
+    while (i>0)
+    {
+        cout<<" "<<i;
+        i--;
+    }
+    cin.get();
+    return 0;
+}
