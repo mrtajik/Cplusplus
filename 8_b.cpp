26e356fb885a8f33c7101c0b2e89e4df3b0db67b
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 10:47:49 -0400

    Continue
    continue

---
+++ 8_b.cpp
@@ -0,0 +1,21 @@
+// continue
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    
+    int i=20;
+    do
+    {
+        cout<<" "<<i;
+        i--;
+        if i==10)
+          continue;
+    } while (i>0);
+    
+    cin.get();
+    return 0;
+}
