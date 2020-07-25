6f4ccb68cba8c7133bf908ca9f858ce57ae151b7
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 09:07:34 -0400

    Random Number Generator
    Random Number Generator

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+// Random Number Generator
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout <<"1-Random Number "<<1+rand() %80 <<endl;
+	cout <<"2-Random Number "<<1+rand() %80 <<endl;
+	cout <<"3-Random Number "<<1+rand() %80 <<endl;
+	cout <<"4-Random Number "<<1+rand() %80 <<endl;
+	cout <<"5-Random Number "<<1+rand() %80 <<endl;
+	cout <<"6-Random Number "<<1+rand() %80 <<endl;
+	
+	return 0;
+}
