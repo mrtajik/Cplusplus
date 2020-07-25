120751523d2d7e067b16e9ca7d541b9df93cae9f
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 17:33:57 -0400

    Array-1D
    1D Array

---
+++ 8_d.cpp
@@ -0,0 +1,19 @@
+// Array 1D
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int A[3]={23,45,1}; // no need to include arrays size if initilized
+    
+    for (int i=0; i>=2; i++)
+    {
+        cout<<"Array : "<<A[i]<<endl;
+    }
+   
+    cin.get();
+    return 0;
+}
