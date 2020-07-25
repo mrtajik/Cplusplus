4979197f61ae22541007db8f3c34bc6254caab32
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 10:35:41 -0400

    Loops-FOR
    FOR

---
+++ 6.cpp
@@ -0,0 +1,18 @@
+// LOOPS
+// FOR
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    // for loop
+    for (int counter =0; counter<=10; counter++)
+    {
+        cout <<" "<<counter;
+    }
+    
+    cin.get();
+    return 0;
+}
