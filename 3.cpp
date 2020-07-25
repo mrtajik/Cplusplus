bf460cb22818a87fab0e8c5e5f0e1554100a5954
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 00:24:36 -0400

    Conditional Statements
    Conditional Statements

---
+++ 3.cpp
@@ -0,0 +1,19 @@
+// Conditional Statements
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int num=10;
+	float num1=21.0;
+
+	if (num==num1)
+	{
+		cout << "   equal";
+	}
+	else
+		cout <<"Not equal";
+	return 0;
+}
