b161c927e1bf0433b0e396728a74de83d77aa26d
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-25 00:10:03 -0400

    Variables
    Variables

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+// Variables
+
+// int - stores integers (whole numbers), without decimals, such as 123 or -123
+// double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+// char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+// string - stores text, such as "Hello World". String values are surrounded by double quotes
+// bool - stores values with two states: true or false
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int num=10;
+	
+	cout << "Num is "<< num;
+
+	return 0;
+}
