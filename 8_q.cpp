ae24b4bdec4d86c596d7208791945fa269d6fdff
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-26 08:10:23 -0400

    OOP, CLASS and Objects
    OOP, CLASS and Objects

---
+++ 8_q.cpp
@@ -0,0 +1,43 @@
+// OOP, CLASS and Objects
+ 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Auto{
+private:
+    int year, month, day;   //only Auto class can see it
+
+public:
+    void message(){
+        cout<<"Testing Class"<<endl;
+    }
+    
+    void set(int date_year,int date_month,int date_day){
+        year=date_year;
+        month=date_month;
+        day=date_day;
+    }
+    
+    void get(){
+        cout<<"Year :"<<year<<endl;
+        cout<<"Month:"<<month<<endl;
+        cout<<"Day  :"<<day<<endl;
+        
+    }
+
+protected:
+    int value3=33;  // can be seen inside THIS class and can be accessed in child class
+};
+
+int main(int argc, const char * argv[])
+{
+    Auto BMW;
+    BMW.message();
+    BMW.set(1985, 12, 31);
+    BMW.get();
+    
+    cin.get();
+    return 0;
+}
